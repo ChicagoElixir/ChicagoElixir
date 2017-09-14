de055425df98a430417519e5172a3faa9e46dd6f
Dorian Karter <dkarter@gmail.com>
2017-09-14 11:00:25 -0500

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -38,15 +38,15 @@ defmodule ChicagoElixir.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.0-rc"},
+      {:phoenix, "~> 1.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.6"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:httpoison, "~> 0.12"},
+      {:phoenix_html, "~> 2.10"},
+      {:phoenix_live_reload, "~> 1.1", only: :dev},
+      {:gettext, "~> 0.13"},
+      {:cowboy, "~> 1.1"},
+      {:httpoison, "~> 0.13"},
       {:poison, "~> 3.1"},
       {:timex, "~> 3.1"},
       {:exfmt, "~> 0.3.0"},
