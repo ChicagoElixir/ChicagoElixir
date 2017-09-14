831b998ec42232f77fb9c713648e50ae46031185
Dorian Karter <dkarter@gmail.com>
2017-09-14 11:01:55 -0500

    Set postgrex version

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule ChicagoElixir.Mixfile do
       {:phoenix, "~> 1.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.13"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.1", only: :dev},
       {:gettext, "~> 0.13"},
